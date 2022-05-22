124b9d57741afb67288579cf2a297f80f78bbeb1
sebokdas1 <sebok.das.ff@gmail.com>
2022-05-22 10:06:38 +0600

    nav home footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home/Home';
+import Navbar from './Shared/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='lg:max-w-7xl mx-auto'>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Shared/Navbar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+// import { signOut } from 'firebase/auth';
+import { Link } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../firebase.init';
+
+const Navbar = () => {
+    const [user, loading, error] = useAuthState(auth);
+
+    // const logOut = () => {
+    //     signOut(auth);
+    //     localStorage.removeItem('accessToken');
+    // }
+
+    const menuItems = <>
+        <li><Link to="/">Home</Link></li>
+        {/* <li><Link to="/appointment">Appointment</Link></li> */}
+        {/* <li><Link to="/reviews">Reviews</Link></li> */}
+        {/* <li><Link to="/contact">Contact Us</Link></li> */}
+        <li><Link to="/about">About</Link></li>
+        {/* {
+            user && <li><Link to="/dashboard">Dashboard</Link></li>
+        }
+        <li>{user ? <button className="btn btn-ghost" onClick={logOut}>Sign Out</button> : <Link to="/login">Login</Link>}</li> */}
+    </>
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {menuItems}
+                    </ul>
+                </div>
+                <Link to="/" className="btn btn-ghost normal-case text-xl">Istock Sources</Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal p-0">
+                    {menuItems}
+                </ul>
+            </div>
+            <div className='navbar-end'>
+                <label tabIndex="1" htmlFor='dashboard-sidebar' className="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                </label>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
