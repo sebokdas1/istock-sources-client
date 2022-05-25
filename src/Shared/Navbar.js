ce6946be2bdcffa544e1e1145d658ae653545d0e
sebokdas1 <sebok.das.ff@gmail.com>
2022-05-25 15:21:57 +0600

    profile updated

--- src/Shared/Navbar.js
+++ src/Shared/Navbar.js
@@ -20,9 +20,6 @@ const Navbar = () => {
 
     const menuItems = <>
         <li><Link to="/">Home</Link></li>
-        {/* <li><Link to="/appointment">Appointment</Link></li> */}
-        {/* <li><Link to="/reviews">Reviews</Link></li> */}
-        {/* <li><Link to="/contact">Contact Us</Link></li> */}
         <li><Link to="/about">About</Link></li>
         {
             user && <li><Link to="/dashboard">Dashboard</Link></li>
