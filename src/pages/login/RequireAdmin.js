3fff487a9b0fd5abf0d53faee017667c5a4768b6
sebokdas1 <sebok.das.ff@gmail.com>
2022-05-26 09:13:44 +0600

    use admin

--- src/pages/login/RequireAdmin.js
+++ src/pages/login/RequireAdmin.js
@@ -1,18 +1,24 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Navigate, useLocation } from 'react-router-dom';
+import auth from '../../firebase.init';
+import Loading from '../../Shared/Loading';
+import useAdmin from '../../Shared/useAdmin';
 
 const RequireAdmin = ({ children }) => {
     const [user, loading] = useAuthState(auth);
-    // const [admin, adminLoading] = useAdmin(user);
+    const [admin, adminLoading] = useAdmin(user);
     const location = useLocation();
 
     if (loading || adminLoading) {
-        return <Loading></Loading>
+        return <Loading />
     }
 
-    // if (!user || !admin) {
-    //     signOut(auth);
-    //     return <Navigate to="/login" state={{ from: location }} replace />
-    // }
+    if (!user || !admin) {
+        signOut(auth);
+        return <Navigate to="/login" state={{ from: location }} replace />
+    }
     return children
 };
 
