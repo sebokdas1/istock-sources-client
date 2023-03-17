8fecb9d5ef2028bc1887ccb2a6a8af3b88e753ba
sebokdas1 <sebok.das.ff@gmail.com>
2023-03-17 19:20:56 +0600

    portfolio internal link to navigate portfolio site

--- src/Shared/Navbar.js
+++ src/Shared/Navbar.js
@@ -1,51 +1,107 @@
-import React from 'react';
-import { signOut } from 'firebase/auth';
-import { Link } from 'react-router-dom';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import auth from '../firebase.init';
+import React from "react";
+import { signOut } from "firebase/auth";
+import { Link } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../firebase.init";
 
 const Navbar = () => {
-    const [user] = useAuthState(auth);
+  const [user] = useAuthState(auth);
 
-    const logOut = () => {
-        signOut(auth);
-        localStorage.removeItem('accessToken');
-    }
+  const logOut = () => {
+    signOut(auth);
+    localStorage.removeItem("accessToken");
+  };
 
-    const menuItems = <>
-        <li className='lg:text-white'><Link to="/">Home</Link></li>
-        <li className='lg:text-white'><Link to="/blogs">Blogs</Link></li>
-        <li className='lg:text-white'><Link to="/my-portfolio">My Portfolio</Link></li>
-        {
-            user && <li className='lg:text-white'><Link to="/dashboard">Dashboard</Link></li>
-        }
-        <li className='lg:text-white'>{user ? <button className="btn btn-ghost" onClick={logOut}>Sign Out</button> : <Link to="/login">Login</Link>}</li>
+  const menuItems = (
+    <>
+      <li className="lg:text-white">
+        <Link to="/">Home</Link>
+      </li>
+      <li className="lg:text-white">
+        <Link to="/blogs">Blogs</Link>
+      </li>
+      <li className="lg:text-white">
+        {/* <Link to="/my-portfolio">My Portfolio</Link> */}
+        <a href="https://sebokdas-33115.web.app" target="_blank">
+          My Portfolio
+        </a>
+      </li>
+      {user && (
+        <li className="lg:text-white">
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      )}
+      <li className="lg:text-white">
+        {user ? (
+          <button className="btn btn-ghost" onClick={logOut}>
+            Sign Out
+          </button>
+        ) : (
+          <Link to="/login">Login</Link>
+        )}
+      </li>
     </>
-    return (
-        <div className="navbar bg-cyan-600 shadow ">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <label tabIndex="0" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabIndex="0" className="menu menu-compact dropdown-content bg-base-100 mt-3 p-2 shadow rounded-box w-52">
-                        {menuItems}
-                    </ul>
-                </div>
-                <Link to="/" className="btn btn-ghost normal-case text-white text-xl">Istock Sources</Link>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu  menu-horizontal p-0">
-                    {menuItems}
-                </ul>
-            </div>
-            {user && <div className='navbar-end'>
-                <label tabIndex="1" htmlFor='dashboard-sidebar' className="btn btn-ghost lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                </label>
-            </div>}
+  );
+  return (
+    <div className="navbar bg-cyan-600 shadow ">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex="0" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex="0"
+            className="menu menu-compact dropdown-content bg-base-100 mt-3 p-2 shadow rounded-box w-52"
+          >
+            {menuItems}
+          </ul>
         </div>
-    );
+        <Link to="/" className="btn btn-ghost normal-case text-white text-xl">
+          Istock Sources
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu  menu-horizontal p-0">{menuItems}</ul>
+      </div>
+      {user && (
+        <div className="navbar-end">
+          <label
+            tabIndex="1"
+            htmlFor="dashboard-sidebar"
+            className="btn btn-ghost lg:hidden"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Navbar;
