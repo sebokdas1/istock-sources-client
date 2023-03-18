32051620a49798c646f622365105eb90807c3f42
sebokdas1 <sebok.das.ff@gmail.com>
2023-03-18 21:41:14 +0600

    contact mail and portfolio added

--- src/Shared/Footer.js
+++ src/Shared/Footer.js
@@ -1,49 +1,63 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    const today = new Date();
-    const year = today.getFullYear();
-    return (
-        <footer className=" p-10">
-            <div className='footer'>
-                <div>
-                    <span className="footer-title">Services</span>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
-                </div>
-                <div>
-                    <span className="footer-title">Company</span>
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
-                </div>
-                <div>
-                    <span className="footer-title">Legal</span>
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
-                </div>
-                <div>
-                    <span className="footer-title">Newsletter</span>
-                    <div className="form-control w-80">
-                        <label className="label">
-                            <span className="label-text">Enter your email address</span>
-                        </label>
-                        <div className="relative">
-                            <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" />
-                            <button className="btn btn-primary absolute top-0 right-0 rounded-l-none">Subscribe</button>
-                        </div>
-                    </div>
-                </div>
+  const today = new Date();
+  const year = today.getFullYear();
+  return (
+    <footer className=" p-10">
+      <div className="footer">
+        <div>
+          <span className="footer-title">Services</span>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </div>
+        <div>
+          <span className="footer-title">Company</span>
+          <a
+            className="link link-hover"
+            target="_blank"
+            href="https://sebokdas-33115.web.app/"
+          >
+            About us
+          </a>
+          <a className="link link-hover" href="mailto:sebok.das66@gmail.com">
+            Contact
+          </a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </div>
+        <div>
+          <span className="footer-title">Newsletter</span>
+          <div className="form-control w-80">
+            <label className="label">
+              <span className="label-text">Enter your email address</span>
+            </label>
+            <div className="relative">
+              <input
+                type="text"
+                placeholder="username@site.com"
+                className="input input-bordered w-full pr-16"
+              />
+              <button className="btn btn-primary absolute top-0 right-0 rounded-l-none">
+                Subscribe
+              </button>
             </div>
-            <div className='text-center my-10'>
-                <p>Copyright &copy; {year} - All right reserved by Istock Sources</p>
-            </div>
-        </footer>
-    );
+          </div>
+        </div>
+      </div>
+      <div className="text-center my-10">
+        <p>Copyright &copy; {year} - All right reserved by Istock Sources</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
