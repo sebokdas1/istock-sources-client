56d934390bca9dfc406763509b67f6f90ad61352
sebokdas1 <sebok.das.ff@gmail.com>
2023-07-13 19:58:59 +0600

    added photo changing slider in banner

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const Banner = () => {
-    return (
-        <div className='mt-3'>
-            <img style={{ borderRadius: '5px' }} src="https://www.smartbd.com/media/magentothem/banner7/xtreme.jpg" alt="" />
-        </div>
-    );
+  const [currentImage, setCurrentImage] = useState(0);
+  const images = [
+    "https://i.ibb.co/kSjrrsd/computer.png",
+    "https://i.ibb.co/ZKHcVbC/2nd.png",
+    "https://i.ibb.co/DQRL07X/3rd.png",
+  ];
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+    }, 3000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [images.length]);
+  return (
+    <div className="mt-3">
+      {/* <img
+        style={{ borderRadius: "5px", width: "100%", height: "80vh" }}
+        src="https://i.ibb.co/kSjrrsd/computer.png"
+        alt=""
+      /> */}
+      <img
+        className="w-[100%] h-[60vh]"
+        src={images[currentImage]}
+        alt={`Image ${currentImage + 1}`}
+      />
+    </div>
+  );
 };
 
 export default Banner;
