85159e173d234b9f9964e718f61b4224c22e18cd
sebokdas1 <sebok.das.ff@gmail.com>
2023-07-15 23:42:07 +0600

    linked all products at home

--- src/pages/Home/Parts.js
+++ src/pages/Home/Parts.js
@@ -2,6 +2,9 @@ import React from "react";
 import { useQuery } from "react-query";
 import Loading from "../../Shared/Loading";
 import Part from "./Part";
+import { BsArrowRight } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import "./Parts.css";
 
 const Parts = () => {
   const { data: parts, isLoading } = useQuery("parts", () =>
@@ -21,6 +24,15 @@ const Parts = () => {
           <Part key={part._id} part={part}></Part>
         ))}
       </div>
+      <Link
+        to="all-parts"
+        className="flex items-center justify-end mt-2 text-xl text-blue-500 pr-5 showAllItem"
+      >
+        <span>Show All</span>
+        <span className="ml-1 partslink">
+          <BsArrowRight />
+        </span>
+      </Link>
     </div>
   );
 };
