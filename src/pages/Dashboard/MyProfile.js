5a06410aaa03a4e747b1fad1b3bc415e5a5bfce4
sebokdas1 <sebok.das.ff@gmail.com>
2023-07-26 13:26:36 +0600

    getting value from update profile form

--- src/pages/Dashboard/MyProfile.js
+++ src/pages/Dashboard/MyProfile.js
@@ -1,13 +1,15 @@
-import React from "react";
-import { useAuthState, useUpdateProfile } from "react-firebase-hooks/auth";
+import React, { useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
-import Loading from "../../Shared/Loading";
+// import Loading from "../../Shared/Loading";
 import { AiOutlineMail, AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import { FaRegEdit } from "react-icons/fa";
 
 const MyProfile = () => {
   const [user] = useAuthState(auth);
-  const [updateProfile, updating, error] = useUpdateProfile(auth);
-  console.log(user);
+  //   const [updateProfile, updating, error] = useUpdateProfile(auth);
+  const [updateAvailable, setupdateAvailable] = useState(false);
+
   const me = {
     displayName: "Sebok Das",
     photoURL: "https://i.ibb.co/Y0XWH6Y/User-Profile-PNG.png",
@@ -17,10 +19,9 @@ const MyProfile = () => {
     linkedin: "https://www.linkedin.com",
     github: "https://www.github.com",
   };
-  const handleUpdate = async (data) => {
+  const handleUpdate = (data) => {
     data.preventDefault();
     const newName = data.target.name.value;
-    const newEmail = data.target.email.value;
     const education = data.target.education.value;
     const location = data.target.location.value;
     const phone = data.target.phone.value;
@@ -28,26 +29,28 @@ const MyProfile = () => {
     const github = data.target.github.value;
     const photo = data.target.photo.value;
 
-    await updateProfile({
-      displayName: newName ? newName : user.displayName,
-      photoURL: photo,
-      phoneNumber: phone,
-      education,
-      email: newEmail ? newEmail : user.email,
-      location,
-      linkedin,
-      github,
-    });
+    const newUserDetails = {
+      displayName: newName ? newName : me.displayName,
+      photoURL: photo ? photo : me.photoURL,
+      phoneNumber: phone ? phone : me.phoneNumber,
+      education: education ? education : me.education,
+      location: location ? location : me.location,
+      linkedin: linkedin ? linkedin : me.linkedin,
+      github: github ? github : me.github,
+    };
+
+    setupdateAvailable(false);
+    console.log(newUserDetails);
   };
-  if (updating) {
-    return <Loading />;
-  }
-  if (error) {
-    console.log(error);
-  }
+
+  //   if (updating) {
+  //     return <Loading />;
+  //   }
+  //   if (error) {
+  //     console.log(error);
+  //   }
   return (
     <div>
-      {/* <h2 className='text-2xl font-bold text-purple-500'>Please Update Your Profile</h2> */}
       <div className="lg:flex  w-[90%] mx-auto gap-4 items-center">
         <div>
           <img
@@ -100,105 +103,114 @@ const MyProfile = () => {
           </div>
         </div>
       </div>
-      <div className="w-2/3 mx-auto mt-4">
-        <form onSubmit={handleUpdate}>
-          <span className="grid lg:grid-cols-2">
-            <label className="block">
-              <span className=" after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-                Name
-              </span>
-              <input
-                type="text"
-                name="name"
-                className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block  rounded-md sm:text-sm focus:ring-1"
-                placeholder={user.displayName}
-              />
-            </label>
-
-            <label className="block">
-              <span className=" after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-                Email
-              </span>
-              <input
-                type="email"
-                name="email"
-                className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block  rounded-md sm:text-sm focus:ring-1"
-                placeholder={user.email}
-              />
-            </label>
-
-            <label className="block">
-              <span className="after:content-['*'] after:ml-0.5 after:text-red-500 text-sm font-medium text-slate-700">
-                Education
-              </span>
-              <input
-                required
-                type="text"
-                name="education"
-                className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block  rounded-md sm:text-sm focus:ring-1"
-                placeholder="BBA/MBAschool/college/university"
-              />
-            </label>
-
-            <label className="block">
-              <span className="after:content-['*'] after:ml-0.5 after:text-red-500 text-sm font-medium text-slate-700">
-                Location
-              </span>
-              <input
-                required
-                type="text"
-                name="location"
-                className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block  rounded-md sm:text-sm focus:ring-1"
-                placeholder="City, Country"
-              />
-            </label>
-
-            <label className="block">
-              <span className="after:content-['*'] after:ml-0.5 after:text-red-500 text-sm font-medium text-slate-700">
-                Phone Number
-              </span>
-              <input
-                required
-                type="text"
-                name="phone"
-                className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block  rounded-md sm:text-sm focus:ring-1"
-                placeholder="01*****"
-              />
-            </label>
-
-            <label className="block">
-              <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-                Linekedin
-              </span>
-              <input
-                required
-                type="text"
-                name="linkedin"
-                className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block  rounded-md sm:text-sm focus:ring-1"
-                placeholder="linkedin profile link"
-              />
-            </label>
-
-            <label className="block">
-              <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-                Photo URL
-              </span>
-              <input
-                required
-                type="text"
-                name="photo"
-                className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block  rounded-md sm:text-sm focus:ring-1"
-                placeholder="photo link"
-              />
-            </label>
+      {!updateAvailable && (
+        <h2
+          className="flex items-center gap-1 text-green-500 justify-center cursor-pointer hover:underline"
+          onClick={() => setupdateAvailable(true)}
+        >
+          Edit profile
+          <span>
+            <FaRegEdit />
           </span>
-          <input
-            type="submit"
-            value="update profile"
-            className="btn btn-primary  mt-3 text-white"
-          />
-        </form>
-      </div>
+        </h2>
+      )}
+
+      {updateAvailable && (
+        <div className="w-2/3 mx-auto mt-5">
+          <form onSubmit={handleUpdate}>
+            <span className="grid lg:grid-cols-2">
+              <label className="block">
+                <span className=" after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+                  Name
+                </span>
+                <input
+                  type="text"
+                  name="name"
+                  className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block  rounded-md sm:text-sm focus:ring-1"
+                  placeholder={user.displayName}
+                />
+              </label>
+
+              <label className="block">
+                <span className=" text-sm font-medium text-slate-700">
+                  Education
+                </span>
+                <input
+                  type="text"
+                  name="education"
+                  className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block  rounded-md sm:text-sm focus:ring-1"
+                  placeholder="BBA/MBAschool/college/university"
+                />
+              </label>
+
+              <label className="block">
+                <span className=" text-sm font-medium text-slate-700">
+                  Location
+                </span>
+                <input
+                  type="text"
+                  name="location"
+                  className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block  rounded-md sm:text-sm focus:ring-1"
+                  placeholder="City, Country"
+                />
+              </label>
+
+              <label className="block">
+                <span className=" text-sm font-medium text-slate-700">
+                  Phone Number
+                </span>
+                <input
+                  type="text"
+                  name="phone"
+                  className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block  rounded-md sm:text-sm focus:ring-1"
+                  placeholder="01*****"
+                />
+              </label>
+
+              <label className="block">
+                <span className=" block text-sm font-medium text-slate-700">
+                  Linekedin
+                </span>
+                <input
+                  type="text"
+                  name="linkedin"
+                  className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block  rounded-md sm:text-sm focus:ring-1"
+                  placeholder="linkedin profile link"
+                />
+              </label>
+
+              <label className="block">
+                <span className=" block text-sm font-medium text-slate-700">
+                  Github
+                </span>
+                <input
+                  type="text"
+                  name="github"
+                  className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block  rounded-md sm:text-sm focus:ring-1"
+                  placeholder="github profile link"
+                />
+              </label>
+
+              <label className="block">
+                <span className=" block text-sm font-medium text-slate-700">
+                  Photo URL
+                </span>
+                <input
+                  type="text"
+                  name="photo"
+                  className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block  rounded-md sm:text-sm focus:ring-1"
+                  placeholder="photo link"
+                />
+              </label>
+            </span>
+            <input
+              type="submit"
+              value="update profile"
+              className="btn btn-primary  mt-3 text-white"
+            />
+          </form>
+        </div>
+      )}
     </div>
   );
 };
