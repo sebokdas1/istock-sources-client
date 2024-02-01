5c4f9df26a06663b22cc78f3b76f27d444c70bca
sebokdas1 <sebok.das.ff@gmail.com>
2024-02-01 21:05:05 +0600

    comment out unwanter console log

--- src/pages/Dashboard/RemoveUserModal.js
+++ src/pages/Dashboard/RemoveUserModal.js
@@ -1,43 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const RemoveUserModal = ({ rmUser, setRMUser }) => {
-    const { email } = rmUser;
-    const [confirm, setConfirm] = useState('');
+  const { email } = rmUser;
+  const [confirm, setConfirm] = useState("");
 
-    const handleUserRemove = (e) => {
-        e.preventDefault()
-        const url = `https://istock-sources-server.onrender.com/user/admin/${email}`;
-        fetch(url, {
-            method: 'DELETE',
-            headers: {
-                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
-            }
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                setRMUser(null)
-            });
-    }
-    return (
-        <div>
-            <input type="checkbox" id="user-delete-modal" className="modal-toggle" />
-            <div className="modal modal-bottom sm:modal-middle">
-                <div className="modal-box">
-                    <label htmlFor="user-delete-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+  const handleUserRemove = (e) => {
+    e.preventDefault();
+    const url = `https://istock-sources-server.onrender.com/user/admin/${email}`;
+    fetch(url, {
+      method: "DELETE",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data)
+        setRMUser(null);
+      });
+  };
+  return (
+    <div>
+      <input type="checkbox" id="user-delete-modal" className="modal-toggle" />
+      <div className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <label
+            htmlFor="user-delete-modal"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label>
 
-                    <h3 className="font-bold text-lg ">please type <small className='text-red-500'>remove</small>  to delete <span className='text-green-500'>{email}</span></h3>
+          <h3 className="font-bold text-lg ">
+            please type <small className="text-red-500">remove</small> to delete{" "}
+            <span className="text-green-500">{email}</span>
+          </h3>
 
-                    <form onSubmit={handleUserRemove} className='grid grid-cols-1 justify-items-center mt-3 gap-4'>
+          <form
+            onSubmit={handleUserRemove}
+            className="grid grid-cols-1 justify-items-center mt-3 gap-4"
+          >
+            <input
+              onChange={(e) => setConfirm(e.target.value)}
+              type="text"
+              name="Cconfirm"
+              className="input input-bordered input-primary w-full max-w-xs"
+              required
+            />
 
-                        <input onChange={(e) => setConfirm(e.target.value)} type="text" name='Cconfirm' className="input input-bordered input-primary w-full max-w-xs" required />
-
-                        <input disabled={confirm !== 'remove'} type="submit" value="drop" className="btn btn-secondary w-full max-w-xs text-white" />
-                    </form>
-                </div>
-            </div>
+            <input
+              disabled={confirm !== "remove"}
+              type="submit"
+              value="drop"
+              className="btn btn-secondary w-full max-w-xs text-white"
+            />
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default RemoveUserModal;
